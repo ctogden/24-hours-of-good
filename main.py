958c2902c1ffef42ffa366575cd8b40b21b60c35
Christopher Ogden <ogdenchris@gmail.com>
2014-02-08 00:37:19 -0500

    Added sessions

--- main.py
+++ main.py
@@ -1,16 +1,52 @@
-"""`main` is the top level module for your Flask application."""
-
-# Import the Flask Framework
 from flask import *
 app = Flask(__name__)
 # Note: We don't need to call run() since our application is embedded within
 # the App Engine WSGI application server.
 
+# Set a secret key for encryping sessions
+app.secret_key = 'adsgjkatQ$WTasW$Twa4tJT$/$3'
+
+
+games = []
+all_games_full = True
+game_counter = -1
+
+class Game:
+    def __init__(self, first_player):
+        global game_counter
+        game_counter += 1
+        self.id = game_counter 
+        self.players = []
+        self.players.append(first_player)
+    def add_player(name):
+        self.players.append(name)
+    def __str__(self):
+        print "Game #" + str(self.id)
 
 @app.route('/')
-def hello():
-    """Return a friendly HTTP greeting."""
-    return 'Hello World!'
+def index():
+    """Create an index page that creates a user session"""
+    global game_counter
+    global all_games_full
+    if session:
+        print "session"
+    else:
+        if all_games_full:
+            session['game'] = game_counter + 1
+            games.append(Game("Hanne"))
+            ##
+            i = 0
+            while i < len(games):
+                games[i].__str__()
+                i += 1
+        else:
+            session['game'] = game_counter + 1
+            games[game_counter].add_player("Chris")
+            i = 0
+            while i < len(games):
+                games[i].__str__()
+                i += 1
+    return str(session['game'])
 
 @app.route('/play')
 def play():
